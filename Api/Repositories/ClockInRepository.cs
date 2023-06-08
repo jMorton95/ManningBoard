8705328542d4b901f3e1dc5b46e5bf43d12dad87
jMorton95 <mortson@hotmail.co.uk>
2023-06-08 16:42:37 +0100

    Refactor DBContext for nullable reference warnings

--- Api/Repositories/ClockInRepository.cs
+++ Api/Repositories/ClockInRepository.cs
@@ -18,7 +18,7 @@ namespace ManningApi.Repositories
 
         public async Task<int> ClockOperatorIn(Operator _operator)
         {
-            ClockModel clockIn = new ClockModel()
+            ClockModel clockIn = new()
             {
                 ClockCardNumber = _operator.ClockCardNumber,
                 OperatorName = _operator.OperatorName,
@@ -32,11 +32,15 @@ namespace ManningApi.Repositories
         }
 
         public void ClockOperatorOut(int clockInId)
-        {
-            ClockModel clock = _context.ClockModel.FirstOrDefault(c => c.ID == clockInId);
-            clock.ClockOutTime = DateTime.Now;
-            _context.ClockModel.Update(clock);
-             _context.SaveChanges();
+        {   
+            ClockModel clock = _context.ClockModel.First(c => c.ID == clockInId);
+
+            if (clock != null)
+            {
+                clock.ClockOutTime = DateTime.Now;
+                _context.ClockModel.Update(clock);
+                _context.SaveChanges();
+            };
         }
 
         public async Task<ClockModel> GetClockById(int clockInId) => await _context.ClockModel.SingleAsync(c => c.ID == clockInId);
--- Api/Repositories/ManningDbContext.cs
+++ Api/Repositories/ManningDbContext.cs
@@ -9,14 +9,14 @@ namespace ManningApi.Repositories
         {
         }
 
-        public DbSet<Operator> Operator { get; set; }
-        public DbSet<Zone> Zone { get; set; }
-        public DbSet<OpStation> OpStation { get; set; }
-        public DbSet<TrainingRequirement> TrainingRequirement { get; set; }
-        public DbSet<TrainingRequirementType> TrainingRequirementsType { get; set; }
-        public DbSet<OperatorCompletedTraining> OperatorCompletedTraining {  get; set; }
-        public DbSet<ShiftType> ShiftType { get; set; }
-        public DbSet<WorkingDayHistory> WorkdayHistory { get; set; }
-        public DbSet<ClockModel> ClockModel { get; set; }
+        public DbSet<Operator>? Operator { get; set; }
+        public DbSet<Zone>? Zone { get; set; }
+        public DbSet<OpStation>? OpStation { get; set; }
+        public DbSet<TrainingRequirement>? TrainingRequirement { get; set; }
+        public DbSet<TrainingRequirementType>? TrainingRequirementsType { get; set; }
+        public DbSet<OperatorCompletedTraining>? OperatorCompletedTraining {  get; set; }
+        public DbSet<ShiftType>? ShiftType { get; set; }
+        public DbSet<WorkingDayHistory>? WorkdayHistory { get; set; }
+        public DbSet<ClockModel>? ClockModel { get; set; }
     }
 }
