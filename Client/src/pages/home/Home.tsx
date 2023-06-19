878503816fa9999a04e1df3a7b429f089bf1f376
jMorton95 <mortson@hotmail.co.uk>
2023-06-19 20:01:38 +0100

    Added rudimentary SignalR implementation in Client

--- Client/src/pages/home/Home.tsx
+++ Client/src/pages/home/Home.tsx
@@ -2,9 +2,52 @@ import { useEffect, useState } from 'react'
 import { AsyncFetchEndpointAndSetState } from '../../services/APIService'
 import Zone from './components/Zone'
 import { type TZone } from '../../types/LineTypes'
+import { type HubConnection, HubConnectionBuilder } from '@microsoft/signalr'
 
 export default function Home(): JSX.Element {
   const [lineZones, setLineZones] = useState<TZone[]>()
+  const [connection, setConnection] = useState<HubConnection | null>(null)
+
+  function handleClick(): void {
+    if (connection != null) {
+      void doStuff(connection)
+    }
+  }
+
+  async function doStuff(connection: HubConnection): Promise<void> {
+    try {
+      await connection.invoke('PushLineState')
+    } catch (error: any) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    const connection = new HubConnectionBuilder()
+      .withUrl('https://localhost:7001/lineHub')
+      .withAutomaticReconnect()
+      .build()
+
+    setConnection(connection)
+  }, [])
+
+  useEffect(() => {
+    if (connection !== null) {
+      connection.start()
+        .then(() => { console.log('Connection Started') })
+        .catch((err: ErrorConstructor) => { console.error(err) })
+
+      connection.on('LineStateUpdate', (lineState: object) => {
+        console.log(lineState)
+      })
+    }
+
+    return () => {
+      if (connection != null) {
+        connection.off('LineStateUpdate')
+      }
+    }
+  }, [connection])
 
   useEffect(() => {
     if (lineZones == null) {
@@ -14,6 +57,7 @@ export default function Home(): JSX.Element {
 
   return (
     <section className={'row'}>
+      <button onClick={handleClick}>Click Me!</button>
       {lineZones?.map((zone) => <Zone {...zone} key={zone.id} />)}
     </section>
   )
