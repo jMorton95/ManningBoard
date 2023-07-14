3c34ab0cc5e713e8a95e633d332954c3e2f4a6e4
PIT-LPT-057\JoshMorton <josh.morton@propeltech.co.uk>
2023-07-14 11:30:14 +0100

    [style] Nested button bugfix

--- Client/src/pages/manning/components/AssignablePopover.tsx
+++ Client/src/pages/manning/components/AssignablePopover.tsx
@@ -22,11 +22,14 @@ export default function AssignablePopover({ stationId, assignType }: AssignableP
 
   return (
     <Popover>
-      <PopoverTrigger>
-        <Button variant={"outline"} size={"sm"}>
-          <img src={add} width={14} alt={flavourText} />
-          {flavourText}
-        </Button>
+      <PopoverTrigger
+        className={`border border-neutral-200 bg-white shadow-sm hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800
+        dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50  h-8 rounded-md px-3 text-xs`}
+      >
+        {/* <Button variant={"outline"} size={"sm"}> */}
+        <img src={add} width={14} alt={flavourText} />
+        {flavourText}
+        {/* </Button> */}
       </PopoverTrigger>
       <PopoverContent className="flex justify-center">
         <AssignableOperators stationId={stationId} assignType={assignType} assignableOperators={assignableOperators} />
